2d7bcfdd2fdf7dfaa971c532b175d51a340891a4
Harry Barber <harrybarber@protonmail.com>
2020-01-04 12:47:52 +0700

    Refactor simulation

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,33 @@ use node::*;
 use tracker::*;
 use wallet::*;
 
-fn main() {
+fn start_simulation(nodes: Vec<Node>, wallet_fan: usize, broadcast_interval_ms: u64) {
     let sys = System::new("experiment");
 
+    // Start nodes
+    let addresses: Vec<_> = nodes.into_iter().map(move |node| node.start()).collect();
+    
+    // Start trackers
+    Tracker::new(addresses.clone()).start();
+
+    // Start wallet
+    Wallet::new(addresses, wallet_fan, broadcast_interval_ms).start();
+
+    // Run experiment
+    sys.run();
+}
+
+fn main() {
     // Start nodes and collect addresses
     let n_nodes = 30;
     let heartbeat_ms = 1_000;
 
-    let mut addresses: Vec<_> = (0..n_nodes)
+    let mut nodes: Vec<_> = (0..n_nodes)
         .map(|_| {
-            let node = Node::new(1, heartbeat_ms, 1, 4);
-            node.start()
+            Node::new(1, heartbeat_ms, 1, 4)
         })
         .collect();
-    let addr = Node::new(2, heartbeat_ms, 1, 4).start();
-    addresses.push(addr);
-    let tracker = Tracker::new(addresses.clone());
-    tracker.start();
-
-    // Wallet
-    let wallet_fan = 12;
-    let wallet = Wallet::new(addresses, wallet_fan, 10);
-    wallet.start();
-
-    // let mut node = Node::new(10, 1000);
-    // node.start();
-    sys.run();
+    nodes.push(Node::new(2, heartbeat_ms, 1, 4));
+    
+    start_simulation(nodes, 10, 100);
 }
