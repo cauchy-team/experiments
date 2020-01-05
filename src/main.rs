91de7fd5f37c17a836de7721c06dcd7aa5f1c45e
Harry Barber <harrybarber@protonmail.com>
2020-01-05 16:23:10 +0700

    Switch to PDE

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod util;
 
 use std::io;
 
-use kernel_density::ecdf::Ecdf;
+use criterion_stats::univariate::{kde::{Kde, Bandwidth, kernel::Gaussian}, Sample};
 use termion::event::Key;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
@@ -23,46 +23,57 @@ struct App {
     sys_addr: SystemAddrs,
     density: Vec<(f64, f64)>,
     window: [f64; 2],
+    range: [f64; 2]
 }
 
 impl App {
-    fn new(sys_addr: SystemAddrs) -> App {
-        let n_nodes = sys_addr.node_addrs.len();
-
+    fn new(sys_addr: SystemAddrs, y_range: u32) -> App {
         let density = (0..32).map(move |i: u32| (i as f64, 0.0)).collect();
         App {
             sys_addr,
             density,
-            window: [0.0, n_nodes as f64],
+            window: [0.0, y_range as f64],
+            range: [0.0, 1.]
         }
     }
 
     async fn update(&mut self) {
         if let Ok(distances) = self.sys_addr.get_all_distances().await {
-            let ecdf = Ecdf::new(&distances);
-            self.window = [0.0, ecdf.max() as f64];
-            self.density = (ecdf.min()..ecdf.max())
-                .map(move |i| (i as f64, ecdf.value(i as u32) as f64))
+            let max = *distances.iter().max().unwrap();
+            let min = *distances.iter().min().unwrap();
+
+            let float_distances: Vec<f64> = distances.into_iter().map(|i| i as f64).collect();
+            let sample = Sample::new(&float_distances);
+            let bandwidth = Bandwidth::Silverman;
+            let kde = Kde::new(sample, Gaussian, bandwidth);
+
+            self.density = (min..max)
+                .map(move |i| {
+                    let point = i as f64;
+                    (point, kde.estimate(point))
+                })
                 .collect();
+            self.range = [0., 0.1]
         }
     }
 }
 
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Start nodes and collect addresses
-    let n_nodes: usize = 300;
+    // Start nodes
+    let n_nodes: usize = 64;
     let heartbeat_ms = 1_000;
+    let sample_size = 16;
 
     let nodes: Vec<_> = (0..n_nodes)
         .map(|_| {
-            let sample_size = 16;
             Node::new(1, heartbeat_ms, 1, sample_size)
         })
         .collect();
 
-    let wallet_broadcast_interval = 1;
-    let sys_addrs = start_simulation(nodes, 10, wallet_broadcast_interval);
+    let wallet_broadcast_interval = 100;
+    let wallet_fan_size = 100;
+    let sys_addrs = start_simulation(nodes, wallet_fan_size, wallet_broadcast_interval);
 
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
@@ -76,7 +87,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let events = Events::new();
 
     // App
-    let mut app = App::new(sys_addrs);
+    let mut app = App::new(sys_addrs, 512);
 
     loop {
         terminal.draw(|mut f| {
@@ -105,8 +116,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .title("Density")
                         .style(Style::default().fg(Color::Gray))
                         .labels_style(Style::default().modifier(Modifier::ITALIC))
-                        .bounds([0., 1.0])
-                        .labels(&["0", "1"]),
+                        .bounds(app.range)
+                        .labels(&["0", "Max"]),
                 )
                 .datasets(&[Dataset::default()
                     .marker(Marker::Dot)
